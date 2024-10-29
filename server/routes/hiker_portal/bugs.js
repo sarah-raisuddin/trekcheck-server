f595e81c04a75bdf2244ecbebd15ba2f0f2a46fd
sarah-raisuddin <sraisuddin28@gmail.com>
2024-10-29 13:45:15 -0700

    added route for bug report

---
+++ server/routes/hiker_portal/bugs.js
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import pool from "../../db.js";
+import { generatePlaceholders } from "../util.js";
+
+const router = Router();
+
+router.post("/bug", async (req, res) => {
+  const fields = ["submittedDate", "bugDescription"];
+  const values = [new Date().toISOString(), req.body.bugDescription];
+
+  if (values.includes(undefined) || values.includes(null)) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const placeholders = generatePlaceholders(fields.length);
+    const query = `INSERT INTO trekcheck.Bugs (${fields.join(
+      ", "
+    )}) VALUES (${placeholders}) RETURNING reportID`;
+    const result = await pool.query(query, values);
+
+    res.status(201).json({
+      message: "Bug report added",
+      reportID: result.rows[0].reportID,
+    });
+  } catch (error) {
+    console.error("Error submitting bug report:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
